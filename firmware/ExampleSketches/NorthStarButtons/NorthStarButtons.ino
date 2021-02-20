7ad809db496f070f529a48ef1fd596e57cd03693
Noah Zerkin <noazark@gmail.com>
2021-02-20 13:43:55 +0800

    Update NorthStarButtons.ino

--- firmware/ExampleSketches/NorthStarButtons/NorthStarButtons.ino
+++ firmware/ExampleSketches/NorthStarButtons/NorthStarButtons.ino
@@ -1,41 +1,74 @@
-#include <Wire.h> // Include the I2C library (required)
-#include <SX1508.h> // Include SX1508 library
+/*
+   Preloaded example sketch for the Combine Reality Deck X Integrator Hub for Project North Star headsets
+   Written by Noah Zerkin in 2020
+    Do as you will with it, which I guess would make it MIT License? Let's go with that.
+   Only stipulation: Don't use it as part of anything that will, by design, hurt anybody.
+   I accept no liability for anything. If you live in a jurisdiction where that won't fly, don't run my poorly-written code.
+   This sketch started out based on an example from the Sparkfun SX1509 library. Enjoy.
+*/
+
+#include <Wire.h> // I2C support library
+
+/*Include SX1508 IO Expander Library. This is a modified version of the Sparkfun SX1509 Library.
+   The SX1508 is the 8-pin varient, which has a different register map than the 16-pin SX1509.
+*/
+#include <SX1508.h>
 #include "Keyboard.h"
+#include <avr/pgmspace.h>
 
-// SX1508 I2C address (set by ADDR1 and ADDR0 (00 by default):
+#define DEBUG 1
+
+// Pin assignments
+#define ARDUINO_INT_PIN 7 // SX1508 int output to D7
+#define FAN 10          //5v fan circuit control (pwm of a MOSFET)
+#define STORAGE 5       //Mass Storage subsystem power control
+#define PORT_1 8        //USB 3 Port 1 power control
+#define PORT_2 12       //USB 3 Port 2 power control
+#define DISP_RST 11     //Display Driver Board reset. Integrator v1.01 and higher
+
+// SX1508 I2C address
 const byte SX1508_ADDRESS = 0x20;  // SX1508 I2C address
-SX1508 io; // Create an SX1508 object to be used throughout
-
-// SX1508 Pins:
-const byte SX1508_BUTTON_PIN = 0; // IO 0 connected to button
-
-// Arduino Pins (not SX1508!)
-const byte ARDUINO_INT_PIN = 7; // SX1508 int output to D7
-
-const byte FAN = 10;          //5v fan circuit control (pwm of a MOSFET)
-const byte STORAGE = 5;       //Mass Storage subsystem power control
-const byte PORT_1 = 8;        //USB 3 Port 1 power control
-const byte PORT_2 = 12;       //USB 3 Port 2 power control
-const int BUTTON_DELAY = 150; //ms to wait for a dual press detection
-const int RESET_DELAY = 2000; //ms to wait before executing dual press function
+SX1508 buttonIO; // Create an SX1508 object for the Button Board
 
 // Global variables:
 bool buttonPressed = false; // Track button press in ISR
+bool buttonsFailed = false; // Let's make sure the button board is actually there
+const short BUTTON_DELAY = 200; //ms to wait for a dual press detection
+const short RESET_DELAY = 2000; //ms to wait before executing dual press function
 
 long buttonTimer[6] = {0, 0, 0, 0, 0, 0};   //place to store the time of each new button press
+short portResetDuration = 500;              //time in ms to hold the reset state of the USB ports
 long portResetTimeout = 0;                  //place to the time when the USB port power should be restored during a reset cycle
 bool portResetting = false;                 //reset in progress boolean
 byte timerMask = 0x00;                      //bits to denote whether a time recorded in the matching slot of buttonTimer is still relevant
 
-bool fanCycling = false;
+bool serialPassthrough = false;
 
-//Keycodes associated with each of the six buttons connected to the button board IO expander
+//Boolean to track if a ButtonFP function should be executed
+bool buttonAction[6] = {0, 0, 0, 0, 0, 0};
+
+//Array of function pointers for each button on the Integrator Button Board
+typedef void (* FP)();
+FP ButtonFP[6] = {&down, &right, &up, &left, &circle, &dot};
+
+//Global bool to turn keycode output on or off, in case we want to switch to a different button function set
+bool keyboardOutput = true;
+
+//Array of keyboard keycodes to be sent for each of the buttons on the Integrator Button Board
 byte keycode[6] = {KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_UP_ARROW, KEY_LEFT_ARROW, 0x5B, 0x5D};
-byte keycode1[6] = {KEY_DOWN_ARROW, KEY_RIGHT_ARROW, KEY_UP_ARROW, KEY_LEFT_ARROW, KEY_RETURN, 0x20};
-byte keycode2[6] = {0xE2, 0xE6, 0xE8, 0xE4, 0x5B, 0x5D};
-
-                                                                
-bool altKeys = false;
+// Keycodes can be ASCII chars or hex, or come from a limited set of preprocessor definitions defined here:
+// https://github.com/arduino-libraries/Keyboard/blob/master/src/Keyboard.h
+// Assuming that you want the buttons on the Integrator Button Board to send keyboard keys,
+// this is someplace where you can keep and easily reassign them.
+// By default, they are assigned to the arrow keys and '[' and ']', used to adjust IPD, eye elevation, and eye relief in the original
+// Project North Star Unity example scene using v1 calibration.
+// The array index for each keycode can coorespond to the the index of the pointer to the function from which you want to send it.
+// That way we can easily change the keycodes in one place, or switch to an alternate keycode set.
+// This can be expanded upon by using a reassignable pointer to this and other arrays of keycodes.
+// Alternatively, we could add an additional dimension to this array.
+//
+// https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
+//
 
 void setup()
 {
@@ -44,64 +77,78 @@ void setup()
   pinMode(PORT_1, OUTPUT);
   pinMode(PORT_2, OUTPUT);
   pinMode(STORAGE, OUTPUT);
-  digitalWrite(PORT_2, LOW);
-  digitalWrite(PORT_1, LOW);
+  pinMode(DISP_RST, OUTPUT);
+
+  //Start with USB 3 ports and onboard mass storage subsystems powered up
+  digitalWrite(PORT_2, HIGH);
+  digitalWrite(PORT_1, HIGH);
   digitalWrite(STORAGE, HIGH);
-  // Serial is used in this example to display the input
-  // value of the SX1508_INPUT_PIN input:
+  digitalWrite(DISP_RST, HIGH);
+
+  // Initialize Serial port for debug output (or to relay serial i/o from display driver board with Integrator v1.01 or greater)
   Serial.begin(9600);
-  // Call io.begin(<address>) to initialize the SX1508. If
+  // Initialize Serial1 port to communicate with display driver board using Integrator v1.01 or greater
+  Serial1.begin(500000);
+
+  // Call buttonIO.begin(<address>) to initialize the SX1508. If
   // it successfully communicates, it'll return 1.
-  if (!io.begin(SX1508_ADDRESS))
+  if (!buttonIO.begin(SX1508_ADDRESS))
   {
     Serial.println("Failed to communicate.");
-    while (1) ;
+    buttonsFailed = true;
+    delay(500);
   }
 
-  // io.configForNorthStarButtons();
+  // buttonIO.configForNorthStarButtons();
 
-  // Use io.pinMode(<pin>, <mode>) to set our button to an
+
+  // Use buttonIO.pinMode(<pin>, <mode>) to set our button to an
   // input with internal pullup resistor activated:
-  // io.writeByte(REG_INPUT_DISABLE, 0b11000000);
-  // io.writeByte(REG_PULL_UP, 0b00111111);
-  //  io.pinMode(SX1508_BUTTON_PIN, INPUT_PULLUP);
-  for (byte i = 0; i < 6; i++) {
-    io.pinMode(i, INPUT_PULLUP);
-    delay(1);
+  // buttonIO.writeByte(REG_INPUT_DISABLE, 0b11000000);
+  // buttonIO.writeByte(REG_PULL_UP, 0b00111111);
+  //  buttonIO.pinMode(SX1508_BUTTON_PIN, INPUT_PULLUP);
+  if (!buttonsFailed) {
+    for (byte i = 0; i < 6; i++) {
+      buttonIO.pinMode(i, INPUT_PULLUP);
+      delay(1);
+    }
+
+    // Use buttonIO.enableInterrupt(<pin>, <signal>) to enable an
+    // interrupt on a pin. The <signal> variable can be either
+    // FALLING, RISING, or CHANGE. Set it to falling, which will
+    // mean the button was pressed:
+    //  for (byte i = 0; i < 5; i++) {
+    //    buttonIO.enableInterrupt(i, FALLING);
+    //    delay(1);
+    //  }
+
+    // Seems I broke the enableInterrupt() function in Sparkfun's SX1509 library when modifying it for SX1508.
+    // Writing to the registers inline for now. - Noah
+    buttonIO.writeByte(0x09, 0b11000000);
+    buttonIO.writeByte(0x0A, 0b00001111);
+    buttonIO.writeByte(0x0B, 0b11111111);
+
+    // The SX1508 has built-in debounce features, so a single
+    // button-press doesn't accidentally create multiple ints.
+    // Use buttonIO.debounceTime(<time_ms>) to set the GLOBAL SX1508
+    // debounce time.
+    // <time_ms> can be either 0, 1, 2, 4, 8, 16, 32, or 64 ms.
+    buttonIO.debounceTime(32); // Set debounce time to 32 ms.
+
+    // After configuring the debounce time, use
+    //debouncePin(<pin>) to enable debounce on an input pin.
+    for (byte i = 0; i < 6; i++) {
+      buttonIO.debouncePin(i);
+    }
+
+    // Attach an Arduino interrupt to the interrupt pin (ARDUINO_INT_PIN). Call
+    // the button function to set the buttonPressed flag whenever the pin goes from HIGH to
+    // LOW.
+    attachInterrupt(digitalPinToInterrupt(ARDUINO_INT_PIN),
+                    button, FALLING);
   }
-
-  // Use io.enableInterrupt(<pin>, <signal>) to enable an
-  // interrupt on a pin. The <signal> variable can be either
-  // FALLING, RISING, or CHANGE. Set it to falling, which will
-  // mean the button was pressed:
-  //  for (byte i = 0; i < 5; i++) {
-  //    io.enableInterrupt(i, FALLING);
-  //    delay(1);
-  //  }
-  io.writeByte(0x09, 0b11000000);
-  io.writeByte(0x0A, 0b00001111);
-  io.writeByte(0x0B, 0b11111111);
-
-  // The SX1508 has built-in debounce features, so a single
-  // button-press doesn't accidentally create multiple ints.
-  // Use io.debounceTime(<time_ms>) to set the GLOBAL SX1508
-  // debounce time.
-  // <time_ms> can be either 0, 1, 2, 4, 8, 16, 32, or 64 ms.
-  io.debounceTime(32); // Set debounce time to 32 ms.
-
-  // After configuring the debounce time, use
-  //debouncePin(<pin>) to enable debounce on an input pin.
-  for (byte i = 0; i < 6; i++) {
-    io.debouncePin(i);
-  }
-
-  // Attach an Arduino interrupt to the interrupt pin. Call
-  // the button function, whenever the pin goes from HIGH to
-  // LOW.
-  attachInterrupt(digitalPinToInterrupt(ARDUINO_INT_PIN),
-                  button, FALLING);
   digitalWrite(STORAGE, HIGH);
-  analogWrite(FAN, 120);
+  analogWrite(FAN, 160);
   digitalWrite(PORT_2, HIGH);
   digitalWrite(PORT_1, HIGH);
   Keyboard.begin();
@@ -110,21 +157,23 @@ void setup()
 
 void loop()
 {
-  //Serial.println(".");
   if (buttonPressed) // If the button() ISR was executed
   {
     buttonPressed = false; // Clear the buttonPressed flag
+#if DEBUG
     Serial.println("Int triggered");
-    // read io.interruptSource() find out which pin generated
+#endif
+    // read buttonIO.interruptSource() find out which pin generated
     // an interrupt and clear the SX1508's interrupt output.
-    byte intStatus = io.interruptSource(true);
-    byte buttonStatus = 0xFF ^ io.readData();
-    // For debugging handiness, print the intStatus variable.
+    byte intStatus = buttonIO.interruptSource(true);
+    byte buttonStatus = 0xFF ^ buttonIO.readData(); // Read and invert button data byte, since buttons are active-low
+    if (!buttonStatus) {
+      Keyboard.releaseAll();
+    }
+#if DEBUG
     // Each bit in intStatus represents a single SX1508 IO.
     Serial.println("intStatus = " + String(intStatus, BIN) + " buttonStatus = " + String(buttonStatus, BIN));
-
-    // If the bit corresponding to our button IO generated
-    // the input:
+#endif
 
     byte pressed = intStatus | buttonStatus;
     byte buttonNum = 0;
@@ -132,9 +181,13 @@ void loop()
     for (byte i = 0; i < 6; i++) {
       if (intStatus & (1 << i)) {
         buttonNum = i;
+#if DEBUG
         Serial.print("Button " + String(buttonNum) + " was ");
+#endif
         if (buttonStatus & (1 << i)) {
+#if DEBUG
           Serial.println("pressed.");
+#endif
 
           //Record time that the button was pressed
           buttonTimer[i] = millis();
@@ -143,22 +196,21 @@ void loop()
           timerMask |= (1 << i);
 
           //Send associated keycode
-          //(For D-Pad. Key sending functions for A and B moved to timer delay to allow for dual press function)
+          //(For D-Pad. Key sending functions for Circle and Dot buttons moved to timer delay to allow for dual press function)
           if (i < 4) {
-            Keyboard.press(keycode[i]);
+            keyAction(i, true);
+            buttonAction[i] = true;
           }
         }
         else {
           //Button triggered interrupt but isn't held... must've been released!
           Serial.println("released. ");
-          Keyboard.release(keycode[i]);
+          keyAction(i, false);
           //"Deactivate" timer
-          if (i >3 && timerMask & (1 << i)) {
-            Keyboard.press(keycode[i]);
-            Keyboard.release(keycode[i]);
-          }
           timerMask &= ~(1 << i);
+#if DEBUG
           Serial.println("It was held for " + String(millis() - buttonTimer[i]) + "ms.");
+#endif
         }
       }
       //How many buttons are held right now?
@@ -166,6 +218,7 @@ void loop()
         numPressed++;
       }
     }
+#if DEBUG
     if (numPressed > 1) {
       Serial.println("There are " + String(numPressed) + " buttons being held.");
     }
@@ -173,8 +226,96 @@ void loop()
       Serial.println();
     }
     Serial.println("TimerMask = " + String(timerMask, BIN));
+#endif
   }
   timerActions();
+  buttonActions();
+  //Relay serial output from display driver if attached (v1.2 only)
+  if (serialPassthrough) {
+    while (Serial1.available()) {
+      Serial.write(Serial1.read());
+    }
+    while (Serial.available()) {
+      Serial1.write(Serial.read());
+    }
+  }
+  else {
+    while (Serial.available()) {
+      char tempByte = Serial.read();
+      switch (tempByte) {
+        case 'r':
+          portReset();
+        case 'd':
+          portDisable();
+          break;
+        case 'e':
+          portEnable();
+          break;
+        case 'm':
+          storageToggle();
+          break;
+        case 's':
+          serialPassthrough = !serialPassthrough;
+        case 'i':
+          digitalWrite(DISP_RST, LOW);
+          delay(10);
+          digitalWrite(DISP_RST, HIGH);
+        default:
+          break;
+      }
+    }
+    if(Serial1.available()){
+      Serial1.flush();
+    }
+  }
+}
+
+void portReset() {
+  digitalWrite(PORT_1, LOW);
+  digitalWrite(PORT_2, LOW);
+  portResetTimeout = millis() + portResetDuration;
+  portResetting = true;
+}
+
+void storageToggle() {
+  bool storageStatus = digitalRead(STORAGE);
+  digitalWrite(STORAGE, !storageStatus);
+  Serial.print("Mass storage turned ");
+  if (storageStatus) {
+    Serial.println("off.");
+  }
+  else {
+    Serial.println("on.");
+  }
+}
+
+void portDisable() {
+  digitalWrite(PORT_1, LOW);
+  digitalWrite(PORT_2, LOW);
+#if DEBUG
+  Serial.println("Ports Disabled");
+#endif
+}
+
+void portEnable() {
+  digitalWrite(PORT_1, HIGH);
+  digitalWrite(PORT_2, HIGH);
+#if DEBUG
+  Serial.println("Ports Enabled");
+#endif
+}
+
+
+//When called, this function either presses or releases a key from the keycode array
+void keyAction(int key, bool pressing) {
+  if (keyboardOutput) {
+    if (pressing) {
+      Keyboard.press(keycode[key]);
+    }
+    else {
+      Keyboard.release(keycode[key]);
+    }
+  }
 }
 
 void timerActions() {
@@ -182,17 +323,23 @@ void timerActions() {
     long currentTime = millis();    //let's just read the time once. No need to keep consulting the hardware clock here
     if ((0b00010000 & timerMask) && !(0b00100000 & timerMask)) {    //Is button A held and button B isnt?
       if (currentTime - BUTTON_DELAY > buttonTimer[4]) {            //Has is been held longer than the time allocated to detect a dual-button press?
-        Keyboard.press(keycode[4]);                                 //Send the key!
+        keyAction(4, 1);                                 //Send the key!
+        buttonAction[4] = true;
         timerMask &= (~(1 << 4));                                   //Kill the timer!
+#if DEBUG
         Serial.println("TimerMask = " + String(timerMask, BIN));    //Tell the story!
+#endif
       }
     }
     else {
       if ((0b00100000 & timerMask) && !(0b00010000 & timerMask)) {  //Same as above... yadda yadda yadda
         if (currentTime - BUTTON_DELAY > buttonTimer[5]) {
-          Keyboard.press(keycode[5]);
+          keyAction(5, 1);
+          buttonAction[5] = true;
           timerMask &= (~(1 << 5));
+#if DEBUG
           Serial.println("TimerMask = " + String(timerMask, BIN));
+#endif
         }
       }
       else {
@@ -200,43 +347,62 @@ void timerActions() {
         if ((0b00100000 & timerMask) && (0b00010000 & timerMask)) { //Are both buttons pressed? Separate comparisons because 0b00110000 & timerMask would evaluate to true for both OR one.
           long initiatedTime = currentTime - RESET_DELAY;                  //Let's count the dual press from when the first button press was detected
           if ((initiatedTime > buttonTimer[4]) && (initiatedTime > buttonTimer[5])) {
-                        digitalWrite(PORT_1, LOW);
-                        digitalWrite(PORT_2, LOW);
-                        portResetTimeout = millis() + 500;
-                        portResetting = true;
-
-            timerMask &= (~(0b11 << 4));
+            portReset();
+            timerMask &= (~(0b11 << 4));  //Kill timers for both A and B
+#if DEBUG
             Serial.println("TimerMask = " + String(timerMask, BIN) + "Port Resetting");
+#endif
           }
         }
       }
     }
   }
+
   if (portResetting && millis() > portResetTimeout) {
     digitalWrite(PORT_1, HIGH);
     digitalWrite(PORT_2, HIGH);
     portResetting = false;
+#if DEBUG
     Serial.println("Port Reset complete");
+#endif
   }
+} //timerActions()
+
+void buttonActions()
+{
+  for (int i = 0; i < 6; i++) {
+    if (buttonAction[i]) {
+      buttonAction[i] = false;
+      ButtonFP[i]();
+    }
+  }
+} //buttonActions()
+
+void up() {
+  //digitalWrite(STORAGE, !digitalRead(STORAGE));   //Toggle Storage Power
 }
 
-void switchAltKeys(){
-  //            if (altKeys) {
-//              altKeys = false;
-//              for (int i = 0; i < 6; i++) {
-//                keycode[i] = keycode1[i];
-//              }
-//              digitalWrite(PORT_1, HIGH);
-//              digitalWrite(PORT_2, HIGH);
-//            }
-//            else {
-//              altKeys = true;
-//              for (int i = 0; i < 6; i++) {
-//                keycode[i] = keycode2[i];
-//              }
-//              digitalWrite(PORT_1, LOW);
-//              digitalWrite(PORT_2, LOW);
-//            }
+void down() {
+
+}
+
+void left() {
+  //digitalWrite(PORT_1, !digitalRead(PORT_1));   //Toggle USB3 Port 1 Power
+}
+
+void right() {
+  //digitalWrite(PORT_2, !digitalRead(PORT_2));   //Toggle USB3 Port 2 Power
+}
+
+void circle() {
+  if(serialPassthrough){
+    serialPassthrough = false;
+    Serial.println("Serial passthrough disabled");
+  }
+}
+ 
+void dot() {
+
 }
 
 // button() is an Arduino interrupt routine, called whenever
